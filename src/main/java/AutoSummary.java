3f99541e0b2f29b010bb1cc23068021c164d8a06
yugj <yugj@yugj-mac-book.local>
2019-04-16 01:42:42 +0800

    auto summary

---
+++ src/main/java/AutoSummary.java
@@ -0,0 +1,238 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * java AutoSummary
+ * -h help
+ * -i ignore files
+ *
+ * @author yugj
+ * @date 19/4/15 20:28.
+ */
+public class AutoSummary {
+
+    private static List<String> ignoredFiles = new ArrayList<String>();
+    private static String basePath;
+
+    /**
+     * SUMMARY.md String
+     */
+    private static StringBuilder summary = new StringBuilder("# Summary").append("\n");
+
+    static {
+        ignoredFiles.add("_book");
+        ignoredFiles.add(".git");
+        ignoredFiles.add(".DS_Store");
+        ignoredFiles.add("node_modules");
+        ignoredFiles.add("SUMMARY.md");
+    }
+
+    /**
+     * 遍历文件
+     *
+     * @param path 文件路径
+     * @throws IOException ex
+     */
+    private void traversalFile(String path) throws IOException {
+        File baseFile = new File(path);
+        File[] files = baseFile.listFiles();
+        if (files == null) {
+            System.out.println("file read error,file path:" + path);
+            System.exit(1);
+        }
+
+
+        //sort
+        List fileList = Arrays.asList(files);
+        Collections.sort(fileList, new Comparator<File>() {
+            public int compare(File o1, File o2) {
+                if (o1.getName().equals("README.md")) {
+                    return -1;
+                }
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for (File file : files) {
+            if (ignoredFiles.contains(file.getName())) {
+                continue;
+            }
+
+            if (file.isFile() && !file.getName().endsWith(".md")) {
+                continue;
+            }
+
+            //当前层级 生成行
+            String alias = generateFileAlias(file);
+            String fileSummaryLine = generateSummaryLine(alias, file.getPath(), alias, 0);
+            summary.append(fileSummaryLine);
+
+            if (file.isFile()) {
+                traversalMd(file);
+            } else {
+                traversalFile(file.getPath());
+            }
+        }
+    }
+
+    /**
+     * 取第一个 H1 标题 取不到则取文件名
+     *
+     * @param file file
+     * @return Heading
+     * @throws IOException io ex
+     */
+    private String generateFileAlias(File file) throws IOException {
+
+        if (file.isDirectory()) {
+            String[] fileSplit = file.getName().split("_");
+            if (fileSplit.length == 2) {
+                return fileSplit[1];
+            } else {
+                return file.getName();
+            }
+        }
+
+        FileReader fr = new FileReader(file);
+        BufferedReader bf = new BufferedReader(fr);
+        String content;
+        while ((content = bf.readLine()) != null) {
+            if (isFirstHeading(content)) {
+                String[] firstHeading = content.split(" ");
+                return firstHeading[1];
+            }
+        }
+        return file.getName();
+    }
+
+    private String generateSummaryLine(String alias, String mdPath, String anchor, int headingLevel) {
+
+        String relatePath = mdPath.replace(basePath, "");
+        int relateLength = appearNumber(relatePath, "/");
+
+        int appendTimes = relateLength + headingLevel;
+
+        String space = getSummaryLineSpace(appendTimes);
+
+        StringBuilder sb = new StringBuilder(space);
+
+
+        File mFile = new File(mdPath);
+        //文件夹路径取文件里面第一个文件作为锚点
+        String firstFileInDir = null;
+        if (mFile.isDirectory()) {
+            File firstFile = mFile.listFiles()[0];
+            firstFileInDir = firstFile.getPath().replace(basePath, "");
+        }
+
+        relatePath = firstFileInDir != null ? firstFileInDir : relatePath;
+
+        sb.append("* [").append(alias).append("]");
+        sb.append("(").append(relatePath).append("#").append(anchor).append(")").append("\n");
+        return sb.toString();
+    }
+
+    private  int appearNumber(String srcText, String findText) {
+        int count = 0;
+        Pattern p = Pattern.compile(findText);
+        Matcher m = p.matcher(srcText);
+        while (m.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    private String getSummaryLineSpace(Integer times) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < times; i++) {
+            sb.append("  ");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * md only
+     *
+     * @param file input file
+     */
+    private void traversalMd(File file) throws IOException {
+
+        if (!file.getName().endsWith(".md")) {
+            return;
+        }
+
+        FileReader fr = new FileReader(file);
+        BufferedReader bf = new BufferedReader(fr);
+        String content;
+
+        boolean firstMatch = false;
+        while ((content = bf.readLine()) != null) {
+            if (isFirstHeading(content)) {
+
+                if (!firstMatch) {
+                    firstMatch = true;
+                    continue;
+                }
+                String[] firstHeading = content.split(" ");
+                String summaryLine = generateSummaryLine(firstHeading[1], file.getPath(), firstHeading[1].toLowerCase(), 1);
+                summary.append(summaryLine);
+            } else if (isSecondHeading(content)) {
+                String[] secondHeading = content.split(" ");
+                String summaryLine = generateSummaryLine(secondHeading[1], file.getPath(), secondHeading[1].toLowerCase(), 2);
+                summary.append(summaryLine);
+            }
+        }
+
+        bf.close();
+        fr.close();
+    }
+
+    /**
+     * 临时用下
+     *
+     * @param content 正文
+     * @return 是否h1
+     */
+    private boolean isFirstHeading(String content) {
+        content = content.trim();
+        return content.startsWith("# ");
+
+    }
+
+    private boolean isSecondHeading(String content) {
+        content = content.trim();
+        return content.startsWith("## ");
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        AutoSummary autoSummary = new AutoSummary();
+        basePath = args[0];
+
+        System.out.println("base path :" + basePath);
+
+        autoSummary.traversalFile(args[0]);
+
+        System.out.println(summary.toString());
+
+
+        String summaryPath = basePath + File.separator + "SUMMARY.md";
+        boolean success = new File(summaryPath).delete();
+        if (!success) {
+            System.out.println("delete summary failed");
+            System.exit(1);
+        }
+
+        FileWriter writer;
+        writer = new FileWriter(basePath + File.separator + "SUMMARY.md");
+        writer.write(summary.toString());
+        writer.flush();
+        writer.close();
+
+        System.exit(0);
+    }
+
+}
